efc7de61da18a250de83318b8b65e5cb10eb15d2
Maxime <maxime.agusti@gmail.com>
2013-03-17 17:47:25 +0100

    17/03

---
+++ JS/table.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(document).ready(function(){
+	$('#x1').mouseenter(function(){
+		$('#x1').fadeTo('fast', 1);
+		});
+	$('#x1').mouseleave(function(){
+		$('#x1').fadeTo('fast', 0.6);
+		});
+	});
